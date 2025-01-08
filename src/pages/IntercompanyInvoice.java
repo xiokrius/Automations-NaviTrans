b5b7c69d195638399487a4093b85986edaac279e
xiokrius <xiokriusa@gmail.com>
2025-01-08 14:57:06 +0300

    fix locators

--- src/pages/IntercompanyInvoice.java
+++ src/pages/IntercompanyInvoice.java
@@ -1,10 +1,11 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import java.time.Duration;
 
 public class IntercompanyInvoice {
 
@@ -13,6 +14,7 @@ public class IntercompanyInvoice {
 
     public IntercompanyInvoice(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Инициализация wait
     }
 
     private void switchToIframe(String iframeXpath) {
@@ -24,8 +26,14 @@ public class IntercompanyInvoice {
     public void fillIntercompanyForm() {
         switchToIframe("/html/body/div[2]/div[2]/div[1]/div/div[1]/div/iframe");
         WebElement obrabotkaButton = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@aria-label='Обработка']")));
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@aria-label=' Обработка']")));
 
         obrabotkaButton.click();
+
+        WebElement rashodiButton = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@aria-label='Расходы']")));
+
+        rashodiButton.click();
+
     }
 }
---
+++ src/tests/TestAllPageFixLocators.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.IntercompanyInvoice;
+import pages.QLoginTest;
+import pages.ZayavkaPage;
+import resources.ConfigManager;
+
+public class TestAllPageFixLocators {
+
+    public static void main(String[] args) {
+        System.out.println("Запуск теста...");
+        // Настройка WebDriver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://192.168.1.13:8080/BC210-TEST/SignIn?ReturnUrl=%2FBC210-TEST%2F");
+
+        String login = ConfigManager.getProperty("inputLogin");
+        String password = ConfigManager.getProperty("inputPassword");
+
+        // Выполнение входа (логин, пароль, нажатие кнопки)
+        QLoginTest loginTest = new QLoginTest(driver);
+        loginTest.inputLogin(login);
+        loginTest.inputPassword(password);
+        loginTest.clickLoginButton();
+
+        // Переход на страницу заявок
+        System.out.println("Переход на страницу заявок...");
+        ZayavkaPage zayavkaPage = loginTest.goToZayavkaPage();
+
+        // Создание новой заявки
+        ZayavkaPage CreateNewOrder = new ZayavkaPage(driver);
+        CreateNewOrder.CreateNewZayavkaCZ();
+        CreateNewOrder.returnToMainContent();
+
+        ZayavkaPage OpenDataOrder = new ZayavkaPage(driver);
+        OpenDataOrder.NewOrderCreate();
+        OpenDataOrder.returnToMainContent();
+
+        IntercompanyInvoice OpenInctercompany = new IntercompanyInvoice(driver);
+        OpenInctercompany.fillIntercompanyForm();
+
+    }
+}
